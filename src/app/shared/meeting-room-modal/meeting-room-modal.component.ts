42f17d2afc3d40c22f35da1314b430aee7cecb3d
Phathompong <maipha1230@gmail.com>
2022-11-24 02:15:45 +0700

    update fixed sidebar and upload meeting room image

--- src/app/shared/meeting-room-modal/meeting-room-modal.component.ts
+++ src/app/shared/meeting-room-modal/meeting-room-modal.component.ts
@@ -1,3 +1,4 @@
+import { MeetingRoomService } from './../../services/meeting-room.service';
 import { MeetingRoomModel } from './../../model/meeting-room.model';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
@@ -12,12 +13,42 @@ export class MeetingRoomModalComponent implements OnInit {
 
   public formMeeting: FormGroup;
 
+  public meetingRoomSizeList: any = []
+  public meetingRoomStatusList: any = []
+
+  public meetingRoom: any = []
+  public gallery:any = []
+  public preview_img: any = []
 
   constructor(private dialogRef: MatDialogRef<MeetingRoomModalComponent>,
-    private formBuilder: FormBuilder) { }
+    private formBuilder: FormBuilder,
+    private meetingRoomService: MeetingRoomService) { }
 
   ngOnInit(): void {
+    this.getMeetingRoomSize();
+    this.getMeetingRoomStatus();
     this.createMeetingForm();
+  }
+
+  getMeetingRoomSize(){
+    this.meetingRoomService.getMeetingRoomSize().subscribe((res: any) => {
+      if (res) {
+        if (res.status == 1) {
+          this.meetingRoomSizeList = res.data
+        }
+      }
+
+    })
+  }
+
+  getMeetingRoomStatus(){
+    this.meetingRoomService.getMeetingRoomStatus().subscribe((res: any) => {
+      if (res) {
+        if (res.status == 1) {
+          this.meetingRoomStatusList = res.data
+        }
+      }
+    })
   }
 
   createMeetingForm(){
@@ -26,9 +57,32 @@ export class MeetingRoomModalComponent implements OnInit {
       roomSize: [null, Validators.required],
       roomCapacity: [null, Validators.required],
       roomStatus: [null, Validators.required],
-      roomGallerty: [null, Validators.required]
+      roomGallery: [null, Validators.required]
     })
   }
+
+  fileChangeEvent(event: any){
+    if (event.target.files.length + this.gallery.length <= 3) {
+      for(let i = 0; i < event.target.files.length; i++){
+        let file: File = event.target.files[i]
+        if (file){
+          let reader = new FileReader();
+          reader.readAsDataURL(file); // read file as data url
+          reader.onload = (event) => { // called once readAsDataURL is completed
+            this.gallery.push(event.target?.result)
+          }
+        }
+      }
+      this.formMeeting.controls['roomGallery'].setValue(this.gallery)
+      console.log(this.formMeeting.controls['roomGallery'].value);
+    } else {
+      console.log("จำกัดมากสุด 3 รูปภาพ");
+
+    }
+
+    }
+
+
 
   onSave(){
     if (this.formMeeting.valid) {
@@ -57,5 +111,4 @@ export class MeetingRoomModalComponent implements OnInit {
 
     return room;
   }
-
 }
