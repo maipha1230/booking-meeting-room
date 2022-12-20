0f0d4fb9c4e75dd091fb6d92698d8e07023fcfab
Phathompong <maipha1230@gmail.com>
2022-12-20 18:40:28 +0700

    update dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+import { DasboardService } from './../../services/dasboard.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import Chart from 'chart.js/auto';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,9 +8,97 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  public lineChart: Chart;
+  public pieChart: Chart;
+  public roomTime: any[] = []
+  public affiliation_count: any[] = []
+  public bookingsOverview: any[] = []
+  public bookingsTotal: number = 0
+  public usersOverview: any[] = []
+  public usersTotal: number = 0
+  public adminsOverview: any[] = []
+  public adminsTotal: number = 0
+  constructor(private dashboardService: DasboardService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.getBookingsOverview();
+    this.getAdminOverview();
+    this.getUserOverview();
+    this.getRoomTime();
+    this.getAffiliationChart();
+  }
+
+
+  getBookingsOverview(){
+    this.dashboardService.getBookingsOverview().subscribe((res: any) => {
+      if (res) {
+        if (res.status == 1) {
+          if (res.data) {
+            this.bookingsOverview = res.data
+          }
+          if (res.total) {
+            this.bookingsTotal = res.total
+          }
+        }
+      }
+    })
+  }
+
+  getUserOverview(){
+    this.dashboardService.getUserOverview().subscribe((res: any) => {
+      if (res) {
+        if (res.status == 1) {
+          if (res.data) {
+            this.usersOverview = res.data
+          }
+          if (res.total) {
+            this.usersTotal = res.total
+          }
+        }
+      }
+    })
+  }
+
+  getAdminOverview(){
+    this.dashboardService.getAdminOverview().subscribe((res: any) => {
+      if (res) {
+        if (res.status == 1) {
+          if (res.data) {
+            this.adminsOverview = res.data
+          }
+          if (res.total) {
+            this.adminsTotal = res.total
+          }
+        }
+      }
+    })
+  }
+
+  getRoomTime(){
+    this.dashboardService.getRoomTime().subscribe((res: any) => {
+      if (res) {
+        if (res.status == 1) {
+          this.roomTime = res.data
+        }
+      }
+    })
+  }
+
+
+  getAffiliationChart(){
+    this.dashboardService.getAffiliationChart().subscribe((res: any) => {
+      if (res) {
+        if (res.status == 1) {
+          this.pieChart = new Chart('pieChart', {
+            type: 'pie',
+            data: res.data
+          })
+          if (res.count) {
+            this.affiliation_count = res.count
+          }
+        }
+      }
+    })
   }
 
 }
---
+++ src/app/services/dasboard.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DasboardService {
+
+  constructor(private http: HttpClient) { }
+
+  getBookingsOverview(): Observable<any> {
+    return this.http.get(`${environment.api_url}/admin/getBookingsOverview`)
+  }
+
+  getUserOverview(): Observable<any> {
+    return this.http.get(`${environment.api_url}/admin/getUserOverview`)
+  }
+
+  getAdminOverview(): Observable<any> {
+    return this.http.get(`${environment.api_url}/admin/getAdminOverview`)
+  }
+
+  getRoomTime():Observable<any> {
+    return this.http.get(`${environment.api_url}/admin/getRoomTime`)
+  }
+
+  getAffiliationChart(): Observable<any> {
+    return this.http.get(`${environment.api_url}/admin/getAffiliationChart`)
+  }
+}
